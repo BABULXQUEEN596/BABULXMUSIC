3527a7bcfaeeb3a8b12b1017529cd436022a2d3f
BABULXQUEEN596 <babuldas826646@gmail.com>
2025-07-30 03:18:16 +0530

    Update play.py

--- AloneX/utils/inline/play.py
+++ AloneX/utils/inline/play.py
@@ -73,7 +73,7 @@ def stream_markup_timer(_, chat_id, played, dur):
         ],
         [
             InlineKeyboardButton(
-                text="• 𝐌ʏ 𝐆ᴏᴅ •", url=f"https://t.me/ll_BABUL_BABY_lll"),
+                text="• 𝐌ʏ 𝐆ᴏᴅ •", url=f"https://t.me/BABUL_ON_TG"),
             InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
     return buttons
@@ -90,7 +90,7 @@ def stream_markup(_, chat_id):
         ],
         [
             InlineKeyboardButton(
-                text="• 𝐌ʏ 𝐆ᴏᴅ •", url=f"https://t.me/ll_BABUL_BABY_lll"),
+                text="• 𝐌ʏ 𝐆ᴏᴅ •", url=f"https://t.me/BABUL_ON_TG"),
             InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
     return buttons
